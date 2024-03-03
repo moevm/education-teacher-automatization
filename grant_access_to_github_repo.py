8613d5ae76512f8c38894fc99a1a637b9a9007a3
MikelazDotPy <onextopplay@gmail.com>
2024-03-03 13:30:22 +0300

    add admin access option

--- grant_access_to_github_repo.py
+++ grant_access_to_github_repo.py
@@ -25,6 +25,7 @@ GIVE = '-g'
 REMOVE = '-r'
 PULL = '-p'
 TOKEN = '-t'
+ADMIN = '-a'
 TOKEN_DEST = 'token'
 TOKEN_HELP = 'take your token from github account'
 ACTION = 'append'
@@ -32,6 +33,7 @@ GIVE_DEST = 'give_access'
 REMOVE_DEST = 'remove_access'
 GIVE_HELP = "add user's accesses to the repo"
 REMOVE_HELP = "withdraw user's accesses to the repo"
+ADMIN_HELP = "add admin for user's accesses to the repo"
 BEGIN_OF_INFO = 13
 LEN_OF_HTTPS = 7
 FAIL_COLOR = '\033[91m'
@@ -52,11 +54,14 @@ def get_args():
     parser.add_argument(PULL,
                         default=False,
                         help=REMOVE_HELP)
+    parser.add_argument(ADMIN,
+                        default=False,
+                        help=ADMIN_HELP)
     results = parser.parse_args()
     return results
 
 
-def give_an_access(collaborator, user_object, repo, pull=False):
+def give_an_access(collaborator, user_object, repo, pull=False, admin=False):
     print("adding {} to collaborators ...".format(collaborator))
     sleep(0.05)
     try:
@@ -66,10 +71,12 @@ def give_an_access(collaborator, user_object, repo, pull=False):
         print("{} does not exist, creating".format(repo))
         repo_object = user_object.create_repo(repo)
     try:
-        if pull:
-             repo_object.add_to_collaborators(collaborator, permission='pull')
+        if admin:
+            repo_object.add_to_collaborators(collaborator, permission='admin')
+        elif pull:
+            repo_object.add_to_collaborators(collaborator, permission='pull')
         else:
-             repo_object.add_to_collaborators(collaborator)
+            repo_object.add_to_collaborators(collaborator)
     except UnknownObjectException:
         print((FAIL_COLOR + "ERROR! {} login contains errors, skipping" + END_COLOR).format(
             collaborator))
@@ -83,10 +90,10 @@ def remove_an_access(collaborator, user_object, repo):
         print("Collaborator {} does not exist or waits for invite".format(collaborator))
 
 
-def change_an_access(users, user_object, repo, give, pull=False):
+def change_an_access(users, user_object, repo, give, pull=False, admin=False):
     for user in users:
         if give:
-            give_an_access(user, user_object, repo, pull)
+            give_an_access(user, user_object, repo, pull, admin)
         else:
             remove_an_access(user, user_object, repo)
 
@@ -110,7 +117,7 @@ def auth(access_token):
         exit()
 
 
-def processing_access_list(github_object, access_list, giving, pull=False):
+def processing_access_list(github_object, access_list, giving, pull=False, admin=False):
     for obj in access_list:
         sleep(0.05)
         repo_link, users = obj.split(':')
@@ -119,17 +126,17 @@ def processing_access_list(github_object, access_list, giving, pull=False):
         print('Owner: {}, repo: {}'.format(repo_owner, repo))
         user_object = get_user_object(github_object, repo_owner)
         sleep(0.05)
-        change_an_access(users.split(','), user_object, repo, giving, pull)
+        change_an_access(users.split(','), user_object, repo, giving, pull, admin)
 
 
 def main():
     args = get_args()
     github_object = auth(args.token)
     give_an_access_list, remove_an_access_list, pull = args.give_access, args.remove_access, args.p
-    processing_access_list(github_object, give_an_access_list, True, pull)
+    admin = args.a
+    processing_access_list(github_object, give_an_access_list, True, pull, admin)
     processing_access_list(github_object, remove_an_access_list, False)
 
 
 if __name__ == "__main__":
     main()
-
